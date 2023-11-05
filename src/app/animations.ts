23fd1f259572ec92882f8b30c10bfba73c0e0482
Luiz Fernando Maehana <luizmaehana@gmail.com>
2023-11-05 20:39:14 -0300

    add shownTrigger and pulseTrigger

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -15,3 +15,27 @@ export const highlightedStateTrigger = trigger('highlightedState', [
     animate(200)
   ])
 ])
+
+export const shownStateTrigger = trigger('shownState', [
+  transition(':enter', [
+    style({
+      opacity: 0
+    }),
+    animate(300, style({
+      opacity: 1
+    }))
+  ]),
+  transition(':leave', [
+    animate(300, style({
+      opacity: 0
+    }))
+  ])
+])
+
+export const pulseCheckTrigger = trigger('pulseCheck', [
+  transition('* => checked', [
+    animate('400ms ease-in', style({
+      transform: 'scale(0.4)'
+    })),
+  ]),
+])
